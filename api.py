9fcee1ed1e833612d4f451b101c2c59b4afd96ff
Jason Cao <oncwnuEjY3kem3WPwI1rLZBTW03g@git.weixin.qq.com>
2023-09-29 11:46:00 +0800

    update

--- api.py
+++ api.py
@@ -1,28 +1,22 @@
 from celery import Celery
-from flask import Flask, jsonify, request, send_from_directory
+from flask import Flask, jsonify, request, send_from_directory, make_response
 from flask_cors import CORS
 from tasks import generate_video
 import time
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 celery = Celery('tasks', broker='redis://localhost:6379/0', result_backend='redis://localhost:6379/0')
 celery.conf.broker_connection_retry_on_startup = True
 celery.conf.worker_concurrency = 2
 
-# @app.route('/generate', methods=['POST'])
-# def generate():
-#     # video_file = request.files['video']
-#     # audio_file = request.files['audio']
-#     video_path = 'input/1.mp4'
-#     audio_path = 'input/1.wav'
-#     # video_file.save(video_path)
-#     # audio_file.save(audio_path)
-    
-#     result = generate_video.delay(video_path, audio_path, 'checkpoints/wav2lip.pth')
-    
-#     return result.id
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', 'https://magic.xinvtech.cn')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,ngrok-skip-browser-warning')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
 
 @app.route('/generate', methods=['POST'])
 def generate():
@@ -31,63 +25,60 @@ def generate():
     data = request.get_json()
     face = data.get('face')
     voice = data.get('voice')
+    model = data.get('model')
+
+    video_path = 'upload/face/' + face
+    audio_path = 'upload/voice/' + voice
+    print(video_path, audio_path)
     
     # result = generate_video.delay(video_path, audio_path, 'checkpoints/wav2lip.pth')
-    task = generate_video.delay()
-    # active, total = get_task_count()
-    active = 1
-    total = 2
-    return {"id": task.id, "work": active, "total": total, "face": face, "voice": voice}
+
+    task = generate_video.delay(video_path, audio_path, model)
+    status, active, total = get_task_count()
+    return {"id": task.id, "face": face, "voice": voice, "status": {"status": status, "work": active, "total": total}}
 
 @app.route('/progress/<task_id>', methods=['GET'])
 def progress(task_id):
     task = celery.AsyncResult(task_id)
-    i = celery.control.inspect()
-    active = list(i.active().values())[0]
-    reserved = list(i.reserved().values())[0]
-    print(active, reserved)
+    # i = celery.control.inspect()
+    # active = list(i.active().values())[0]
+    # reserved = list(i.reserved().values())[0]
+    # print(active, reserved)
+    print(task.status)
     if task.ready():
-        status = task.state
+        # 获取任务返回值
+        task_result = task.get()
+        response = {'status': 'SUCCESS', 'video': task_result}
+        # response = {'status': 'SUCCESS'}
     else:
-        status = task.state
-    return jsonify({'status': status})
+        response = {'status': 'PENDING'}
 
-
-    # if task.state == 'PENDING':
-    #     response = {'status': 'waiting', 'progress': 0}
-    # elif task.state == 'STARTED':
-    #     response = {'status': 'working', 'progress': task.info.get('progress', 0)}
-    # elif task.state == 'SUCCESS':
-    #     response = {'status': 'finish', 'progress': 100}
-    # else:
-    #     response = {'status': 'error', 'message': task.info.get('error', 'Unknown error')}
-
-    # return jsonify(response)
+    return response
+    
 
 @app.route('/uploadface', methods=['POST'])
 def uploadface():
     files = request.files.getlist('file')
     timestamp = int(round(time.time() * 1000))
-    print(timestamp)
-    print(files)
+
     for file in files:
         extension = file.filename.split('.')[-1]
         filename = str(timestamp) + "." + extension
         file.save("upload/face/" + filename)
-    
+        
     return filename
+        
 
 @app.route('/uploadvoice', methods=['POST'])
 def uploadvoice():
     files = request.files.getlist('file')
     timestamp = int(round(time.time() * 1000))
-    print(timestamp)
-    print(files)
+
     for file in files:
         extension = file.filename.split('.')[-1]
         filename = str(timestamp) + "." + extension
         file.save("upload/voice/" + filename)
-    
+        
     return filename
 
 @app.route('/getface/<filename>', methods=['GET'])
@@ -99,13 +90,28 @@ def getface(filename):
 def getvoice(filename):
     print(filename)
     return send_from_directory("upload/voice/", filename)
+
+@app.route('/getresult/<filename>', methods=['GET'])
+def getresult(filename):
+    print(filename)
+    return send_from_directory("results/", filename)
+
+@app.route('/status', methods=['GET'])
+def getStatus():
+    status, active, total = get_task_count()
+    return {"status": status, "work": active, "total": total}
     
 
 def get_task_count():
-    i = celery.control.inspect()
-    active = list(i.active().values())[0]
-    reserved = list(i.reserved().values())[0]
-    return len(active), len(reserved) + len(active)
+    try:
+        i = celery.control.inspect()
+        active = list(i.active().values())[0]
+        reserved = list(i.reserved().values())[0]
+        return "RUNNING", len(active), len(reserved) + len(active)
+        
+    except:
+        return "ERROR", 0, 0
+    
 
 
 
--- tasks.py
+++ tasks.py
@@ -1,15 +1,27 @@
-from celery import Celery
+from celery import Celery, current_task
 import time
+import subprocess
+
 
 celery = Celery('tasks', broker='redis://localhost:6379/0', result_backend='redis://localhost:6379/0')
 celery.conf.broker_connection_retry_on_startup = True
 celery.conf.worker_concurrency = 2
 
 @celery.task
-def generate_video():
-    total_time = 60
-    for i in range(total_time):
-        time.sleep(1)
-        progress = (i + 1) / total_time * 100
-        print(f"Task progress: {progress}%")
-    return "test"
+def generate_video(video_path, audio_path, model):
+    task_id = current_task.request.id
+
+    # total_time = 20
+    # for i in range(total_time):
+    #     time.sleep(1)
+    #     progress = (i + 1) / total_time * 100
+
+    #     print(f"Task progress: {progress}%")
+
+    result_path = f'results/{task_id}.mp4'
+    print(task_id, model)
+    
+    command = f'python inference.py --checkpoint_path checkpoints/{model} --face {video_path} --audio {audio_path} --outfile {result_path}'
+    subprocess.run(command, shell=True)
+
+    return f"{task_id}.mp4"
